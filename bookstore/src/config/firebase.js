8e3f387a506257ecdecb2114648e59de4e22d976
Capacitaciones <gamethef@outlook.com>
2018-04-21 16:35:21 +0000

    s

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyAcXZuMk7P_puFA_jwhvXmvruRFaW3c3YM",
+    authDomain: "bookstore-abb28.firebaseapp.com",
+    databaseURL: "https://bookstore-abb28.firebaseio.com",
+    projectId: "bookstore-abb28",
+    storageBucket: "bookstore-abb28.appspot.com",
+    messagingSenderId: "736675358562"
+}
